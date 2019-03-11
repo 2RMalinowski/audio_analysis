b4655cf1123b836a3bf6225e94f6ecf5d502b0c4
Robb <2rmalinowski@gmail.com>
2019-03-11 15:36:16 +0100

    detect_broken_record_from() before complete

--- operations.py
+++ operations.py
@@ -5,6 +5,15 @@ import storage
 import ui
 
 
+def detect_broken_record_from(file_list):
+    for file in file_list:
+        fs, data = wavfile.read(file)
+        #  if condition:
+        ui.display_message(f'{file} invalid {index_of_first_dropped_sample}')
+        #  else:
+        ui.display_message(f'{file} valid')
+
+
 def save_waveforms_from(file_list):
     number_rows = len(file_list)
     number_cols = 1
