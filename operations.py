9390adce1224ebdc01dd155550d92a6457f2f559
Robb <2rmalinowski@gmail.com>
2019-03-11 15:33:29 +0100

    save_waveforms() refactored

--- operations.py
+++ operations.py
@@ -2,22 +2,17 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
 import storage
-
-
-# def detect_broken_record_from(file_list):
-#     for file in file_list:
-
-#     pass
+import ui
 
 
 def save_waveforms_from(file_list):
-    nrows = len(file_list)
-    ncols = 1
+    number_rows = len(file_list)
+    number_cols = 1
     index = 1
     data = f'data{index}'
     for file in file_list:
         fs, data = wavfile.read(file)
-        plt.subplot(f'{nrows}{ncols}{index}')
+        plt.subplot(f'{number_rows}{number_cols}{index}')
         plt.plot(data)
         plt.title(file)
         index += 1
--- ui.py
+++ ui.py
@@ -3,5 +3,6 @@ def display_program_menu(menu_commands):
     for option in menu_commands:
         print(f'({option[0]}) {option}')  # initial letter in brackets and option name
 
+
 def display_message(message):
     print(message)
